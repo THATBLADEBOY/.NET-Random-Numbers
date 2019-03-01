4fb4232706962593d122c07f063d2a2bcb2bb281
THATBLADEBOY <aus10b@gmail.com>
2019-03-01 15:41:37 -0600

    initial commit; practice complete

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace random_numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<int> numbers = new List<int> {
+                random.Next(10),
+                random.Next(10),
+                random.Next(10),
+                random.Next(10),
+                random.Next(10),
+            };
+
+            for(int i=0; i<numbers.Count - 1; i++) {
+                if(i == numbers[i]) {
+                    Console.WriteLine($"numbers list contains {i}");
+                } else {
+                    Console.WriteLine($"numbers list does not contain {i}");
+                }
+            }
+
+            
+        }
+    }
+}
